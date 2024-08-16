0d24bdf401c8faac1ddf7d85a0c55844318a6f00
Shazil Hamzah <shazil.hamzah@outlook.com>
2024-08-16 23:20:04 +0500

    jwt secret error fixed

--- models/User.js
+++ models/User.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 const jwt = require('jsonwebtoken');
 require("dotenv").config();
-const JWT_SECRET = process.env.JWT_SECRET;
+const JWT_SECRET = process.env.REACT_APP_JWT_SECRET;
 
 const userSchema = new Schema({
   name: {
